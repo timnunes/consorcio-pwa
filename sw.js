03e170832a3b30827cfe0a10cf4672ed0f96d87b
timnunes <timnunes@users.noreply.github.com>
2026-06-11 14:22:20 +0000

    deploy: ffb279810522218e727f39bdb1111be51650d943

--- sw.js
+++ sw.js
@@ -1,36 +1,23 @@
 importScripts('config.js');
-
 const CACHE_NAME = CONFIG.appShortName + "-" + CONFIG.cacheVersion;
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/config.js',
-  '/app.js',
-  '/style.css',
-  CONFIG.appIcon,
+  './',
+  './index.html',
+  './manifest.json',
+  './config.js',
+  './app.js',
+  './style.css',
+  './icon-192.png',
+  './icon-512.png',
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+      .then(() => self.skipWaiting())
   );
 });
 
@@ -45,6 +32,24 @@ self.addEventListener('activate', event => {
           }
         })
       );
-    }).then(() => self.clients.claim())
+    }).then(() => {
+      self.clients.claim();
+      // Avisa todas as abas que tem versão nova
+      self.clients.matchAll({ type: 'window' }).then(clients => {
+        clients.forEach(client => client.postMessage({ type: 'SW_UPDATED' }));
+      });
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
